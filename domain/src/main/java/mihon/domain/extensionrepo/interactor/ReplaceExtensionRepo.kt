e2868176f0f7103815afbfd927efa29e03307039
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 17:42:18 +0100

    ReplaceExtensionRepo.kt

--- domain/src/main/java/mihon/domain/extensionrepo/interactor/ReplaceExtensionRepo.kt
+++ domain/src/main/java/mihon/domain/extensionrepo/interactor/ReplaceExtensionRepo.kt
@@ -1,11 +1,18 @@
-package mihon.domain.extensionrepo.interactor
+package akojdad.domain.extensionrepo.interactor
 
-import mihon.domain.extensionrepo.model.ExtensionRepo
-import mihon.domain.extensionrepo.repository.ExtensionRepoRepository
+import akojdad.domain.extensionrepo.model.ExtensionRepo
+import akojdad.domain.extensionrepo.repository.ExtensionRepoRepository
 
+/**
+ * مسؤول عن استبدال مستودع بإصدار جديد أو تحديث بياناته.
+ */
 class ReplaceExtensionRepo(
     private val repository: ExtensionRepoRepository,
 ) {
+    /**
+     * استبدال مستودع بناءً على البيانات الجديدة.
+     * @param repo نموذج بيانات المستودع الجديد.
+     */
     suspend fun await(repo: ExtensionRepo) {
         repository.replaceRepo(repo)
     }
