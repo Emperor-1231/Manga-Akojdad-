85ff2ca305f271b24df0f54b94c9e4b9c69c9580
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 17:26:24 +0100

    ExtensionRepo.kt

--- domain/src/main/java/mihon/domain/extensionrepo/model/ExtensionRepo.kt
+++ domain/src/main/java/mihon/domain/extensionrepo/model/ExtensionRepo.kt
@@ -1,5 +1,14 @@
 package mihon.domain.extensionrepo.model
 
+/**
+ * نموذج بيانات يمثل مستودع الإضافات في تطبيق Manga Akojdad.
+ *
+ * @param baseUrl عنوان الموقع الأساسي للمستودع.
+ * @param name اسم المستودع.
+ * @param shortName الاسم القصير للمستودع (اختياري).
+ * @param website رابط الموقع الإلكتروني للمستودع.
+ * @param signingKeyFingerprint بصمة المفتاح الخاص بالمستودع.
+ */
 data class ExtensionRepo(
     val baseUrl: String,
     val name: String,
