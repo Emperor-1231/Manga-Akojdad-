1c576529492730bcbe221c982820a1726531c1d1
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 17:17:57 +0100

    GetUpcomingManga.kt

--- domain/src/main/java/mihon/domain/upcoming/interactor/GetUpcomingManga.kt
+++ domain/src/main/java/mihon/domain/upcoming/interactor/GetUpcomingManga.kt
@@ -5,15 +5,26 @@ import kotlinx.coroutines.flow.Flow
 import tachiyomi.domain.manga.model.Manga
 import tachiyomi.domain.manga.repository.MangaRepository
 
+/**
+ * Interactor responsible for retrieving the upcoming mangas for "Manga Akojdad".
+ *
+ * @property mangaRepository Repository to fetch manga data.
+ */
 class GetUpcomingManga(
     private val mangaRepository: MangaRepository,
 ) {
 
+    // Define the statuses of mangas that are considered "upcoming" or ongoing
     private val includedStatuses = setOf(
         SManga.ONGOING.toLong(),
         SManga.PUBLISHING_FINISHED.toLong(),
     )
 
+    /**
+     * Retrieves a flow of upcoming mangas based on the status of ongoing or recently finished mangas.
+     *
+     * @return A flow of a list of upcoming mangas.
+     */
     suspend fun subscribe(): Flow<List<Manga>> {
         return mangaRepository.getUpcomingManga(includedStatuses)
     }
