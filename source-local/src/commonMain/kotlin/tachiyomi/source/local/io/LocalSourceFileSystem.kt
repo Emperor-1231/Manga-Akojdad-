e36e0d34189287a2a4fac0c8dc7b0ed414a9eee3
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 16:48:51 +0100

    Update LocalSourceFileSystem.kt

--- source-local/src/commonMain/kotlin/tachiyomi/source/local/io/LocalSourceFileSystem.kt
+++ source-local/src/commonMain/kotlin/tachiyomi/source/local/io/LocalSourceFileSystem.kt
@@ -1,14 +1,18 @@
-package tachiyomi.source.local.io
+package com.mangaakojdad.source.local.io
 
 import com.hippo.unifile.UniFile
 
 expect class LocalSourceFileSystem {
 
+    // الحصول على المجلد الأساسي
     fun getBaseDirectory(): UniFile?
 
+    // الحصول على الملفات داخل المجلد الأساسي
     fun getFilesInBaseDirectory(): List<UniFile>
 
+    // الحصول على مجلد المانجا بالاسم المحدد
     fun getMangaDirectory(name: String): UniFile?
 
+    // الحصول على الملفات داخل مجلد المانجا
     fun getFilesInMangaDirectory(name: String): List<UniFile>
 }
