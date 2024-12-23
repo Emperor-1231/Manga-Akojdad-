827edcd042102a077fb648702e6cd355559ecb7d
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 16:35:18 +0100

    build.gradle.kts

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,23 +1,23 @@
 plugins {
-    id("mihon.library")
-    kotlin("android")
-    kotlin("plugin.serialization")
-    alias(libs.plugins.sqldelight)
+    id("mihon.library") // استخدام مكتبة Mihon الخاصة
+    kotlin("android") // Kotlin لـ Android
+    kotlin("plugin.serialization") // التفعيل لـ Kotlin Serialization
+    alias(libs.plugins.sqldelight) // استخدام SQDelight لإنشاء قاعدة بيانات
 }
 
 android {
-    namespace = "tachiyomi.data"
+    namespace = "com.mangaakojdad.data" // تغيير الـ namespace ليتناسب مع التطبيق
 
     defaultConfig {
-        consumerProguardFiles("consumer-rules.pro")
+        consumerProguardFiles("consumer-rules.pro") // إضافة ملف proguard الخاص بالتطبيق
     }
 
     sqldelight {
         databases {
-            create("Database") {
-                packageName.set("tachiyomi.data")
-                dialect(libs.sqldelight.dialects.sql)
-                schemaOutputDirectory.set(project.file("./src/main/sqldelight"))
+            create("MangaDatabase") { // إنشاء قاعدة بيانات بالاسم الصحيح
+                packageName.set("com.mangaakojdad.data") // تغيير الـ package name ليتناسب مع التطبيق
+                dialect(libs.sqldelight.dialects.sql) // تحديد الدياكتيك الخاص بـ SQLDelight
+                schemaOutputDirectory.set(project.file("./src/main/sqldelight")) // تعيين دليل المخطط
             }
         }
     }
@@ -25,14 +25,14 @@ android {
 
 kotlin {
     compilerOptions {
-        freeCompilerArgs.add("-opt-in=kotlinx.serialization.ExperimentalSerializationApi")
+        freeCompilerArgs.add("-opt-in=kotlinx.serialization.ExperimentalSerializationApi") // تمكين خاصية التجربة لـ Kotlin Serialization
     }
 }
 
 dependencies {
-    implementation(projects.sourceApi)
-    implementation(projects.domain)
-    implementation(projects.core.common)
+    implementation(projects.sourceApi) // إضافة مكتبة الـ API المصدر
+    implementation(projects.domain) // إضافة مكتبة الـ Domain
+    implementation(projects.core.common) // إضافة المكتبة المشتركة
 
-    api(libs.bundles.sqldelight)
+    api(libs.bundles.sqldelight) // إضافة مكتبة SQDelight
 }
