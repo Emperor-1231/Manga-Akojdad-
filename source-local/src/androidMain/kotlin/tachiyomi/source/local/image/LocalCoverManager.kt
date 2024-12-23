d2e8db11fc80e820d1085b02c8a52952c65aa48c
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 17:10:53 +0100

    LocalCoverManager.kt

--- source-local/src/androidMain/kotlin/tachiyomi/source/local/image/LocalCoverManager.kt
+++ source-local/src/androidMain/kotlin/tachiyomi/source/local/image/LocalCoverManager.kt
@@ -1,50 +1,38 @@
 package tachiyomi.source.local.image
 
 import android.content.Context
-import com.hippo.unifile.UniFile
-import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.util.storage.DiskUtil
-import tachiyomi.core.common.storage.nameWithoutExtension
-import tachiyomi.core.common.util.system.ImageUtil
+import tachiyomi.domain.manga.model.SManga
 import tachiyomi.source.local.io.LocalSourceFileSystem
+import tachiyomi.core.common.storage.extension
+import tachiyomi.core.common.util.system.ImageUtil
+import tachiyomi.core.common.util.system.logcat
+import logcat.LogPriority
+import com.hippo.unifile.UniFile
+import tachiyomi.source.local.metadata.fillMetadata
 import java.io.InputStream
 
-private const val DEFAULT_COVER_NAME = "cover.jpg"
-
-actual class LocalCoverManager(
+class LocalCoverManager(
     private val context: Context,
-    private val fileSystem: LocalSourceFileSystem,
+    private val fileSystem: LocalSourceFileSystem
 ) {
-
-    actual fun find(mangaUrl: String): UniFile? {
-        return fileSystem.getFilesInMangaDirectory(mangaUrl)
-            // Get all file whose names start with "cover"
-            .filter { it.isFile && it.nameWithoutExtension.equals("cover", ignoreCase = true) }
-            // Get the first actual image
-            .firstOrNull { ImageUtil.isImage(it.name) { it.openInputStream() } }
+    fun find(mangaName: String): UniFile? {
+        val mangaDir = fileSystem.getMangaDirectory(mangaName) ?: return null
+        return findCoverInDirectory(mangaDir)
     }
 
-    actual fun update(
-        manga: SManga,
-        inputStream: InputStream,
-    ): UniFile? {
-        val directory = fileSystem.getMangaDirectory(manga.url)
-        if (directory == null) {
-            inputStream.close()
-            return null
+    private fun findCoverInDirectory(mangaDir: UniFile): UniFile? {
+        val coverFile = mangaDir.listFiles().find {
+            ImageUtil.isImage(it.name) { it.openInputStream() }
         }
+        return coverFile
+    }
 
-        val targetFile = find(manga.url) ?: directory.createFile(DEFAULT_COVER_NAME)!!
-
-        inputStream.use { input ->
-            targetFile.openOutputStream().use { output ->
-                input.copyTo(output)
-            }
+    fun update(manga: SManga, coverInputStream: InputStream) {
+        val mangaDir = fileSystem.getMangaDirectory(manga.url) ?: return
+        val coverFile = mangaDir.createFile("cover.jpg")
+        coverFile?.openOutputStream()?.use { outputStream ->
+            coverInputStream.copyTo(outputStream)
         }
-
-        DiskUtil.createNoMediaFile(directory, context)
-
-        manga.thumbnail_url = targetFile.uri.toString()
-        return targetFile
+        manga.thumbnail_url = coverFile?.uri.toString()
     }
 }
