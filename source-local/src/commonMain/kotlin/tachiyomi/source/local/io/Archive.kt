024b8debbfcd2ee3fd3b0721b12029fabf590a87
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 16:47:56 +0100

    Archive.kt

--- source-local/src/commonMain/kotlin/tachiyomi/source/local/io/Archive.kt
+++ source-local/src/commonMain/kotlin/tachiyomi/source/local/io/Archive.kt
@@ -1,12 +1,14 @@
-package tachiyomi.source.local.io
+package com.mangaakojdad.source.local.io
 
 import com.hippo.unifile.UniFile
 import tachiyomi.core.common.storage.extension
 
 object Archive {
 
+    // أنواع الأرشيف المدعومة
     private val SUPPORTED_ARCHIVE_TYPES = listOf("zip", "cbz", "rar", "cbr", "7z", "cb7", "tar", "cbt")
 
+    // التحقق مما إذا كان الملف من نوع أرشيف مدعوم
     fun isSupported(file: UniFile): Boolean {
         return file.extension?.lowercase() in SUPPORTED_ARCHIVE_TYPES
     }
