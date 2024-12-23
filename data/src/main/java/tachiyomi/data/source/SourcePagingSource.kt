dfa7adfa31a6be823203998f0ca78059eecae30c
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 15:27:14 +0100

    SourcePagingSource.kt

--- data/src/main/java/tachiyomi/data/source/SourcePagingSource.kt
+++ data/src/main/java/tachiyomi/data/source/SourcePagingSource.kt
@@ -7,27 +7,28 @@ import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.SManga
 import tachiyomi.core.common.util.lang.withIOContext
 import tachiyomi.domain.source.repository.SourcePagingSourceType
+import tachiyomi.domain.source.model.MangaAkojdadManga
 
-class SourceSearchPagingSource(source: CatalogueSource, val query: String, val filters: FilterList) :
-    SourcePagingSource(source) {
+class MangaAkojdadSearchPagingSource(source: CatalogueSource, val query: String, val filters: FilterList) :
+    MangaAkojdadPagingSource(source) {
     override suspend fun requestNextPage(currentPage: Int): MangasPage {
         return source.getSearchManga(currentPage, query, filters)
     }
 }
 
-class SourcePopularPagingSource(source: CatalogueSource) : SourcePagingSource(source) {
+class MangaAkojdadPopularPagingSource(source: CatalogueSource) : MangaAkojdadPagingSource(source) {
     override suspend fun requestNextPage(currentPage: Int): MangasPage {
         return source.getPopularManga(currentPage)
     }
 }
 
-class SourceLatestPagingSource(source: CatalogueSource) : SourcePagingSource(source) {
+class MangaAkojdadLatestPagingSource(source: CatalogueSource) : MangaAkojdadPagingSource(source) {
     override suspend fun requestNextPage(currentPage: Int): MangasPage {
         return source.getLatestUpdates(currentPage)
     }
 }
 
-abstract class SourcePagingSource(
+abstract class MangaAkojdadPagingSource(
     protected val source: CatalogueSource,
 ) : SourcePagingSourceType() {
 
