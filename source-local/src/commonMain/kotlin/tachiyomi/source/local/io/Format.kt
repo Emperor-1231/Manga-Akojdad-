f4e4a107c105dd4bd4e554bcb0f923d81efa9cd0
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 16:48:37 +0100

    Format.kt

--- source-local/src/commonMain/kotlin/tachiyomi/source/local/io/Format.kt
+++ source-local/src/commonMain/kotlin/tachiyomi/source/local/io/Format.kt
@@ -1,18 +1,25 @@
-package tachiyomi.source.local.io
+package com.mangaakojdad.source.local.io
 
 import com.hippo.unifile.UniFile
 import tachiyomi.core.common.storage.extension
-import tachiyomi.source.local.io.Archive.isSupported as isArchiveSupported
+import com.mangaakojdad.source.local.io.Archive.isSupported as isArchiveSupported
 
 sealed interface Format {
+    // تنسيق المجلد
     data class Directory(val file: UniFile) : Format
+
+    // تنسيق الأرشيف
     data class Archive(val file: UniFile) : Format
+
+    // تنسيق epub
     data class Epub(val file: UniFile) : Format
 
+    // استثناء التنسيق غير المعروف
     class UnknownFormatException : Exception()
 
     companion object {
 
+        // تحديد نوع الملف
         fun valueOf(file: UniFile) = when {
             file.isDirectory -> Directory(file)
             file.extension.equals("epub", true) -> Epub(file)
