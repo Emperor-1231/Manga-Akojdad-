64553675a41099cc6eed392267236fa259f64e05
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 14:40:13 +0100

    CategoryRepositoryImpl.kt

--- data/src/main/java/tachiyomi/data/category/CategoryRepositoryImpl.kt
+++ data/src/main/java/tachiyomi/data/category/CategoryRepositoryImpl.kt
@@ -11,30 +11,36 @@ class CategoryRepositoryImpl(
     private val handler: DatabaseHandler,
 ) : CategoryRepository {
 
+    // الحصول على فئة باستخدام المعرف
     override suspend fun get(id: Long): Category? {
         return handler.awaitOneOrNull { categoriesQueries.getCategory(id, ::mapCategory) }
     }
 
+    // الحصول على جميع الفئات
     override suspend fun getAll(): List<Category> {
         return handler.awaitList { categoriesQueries.getCategories(::mapCategory) }
     }
 
+    // الحصول على جميع الفئات كـ Flow
     override fun getAllAsFlow(): Flow<List<Category>> {
         return handler.subscribeToList { categoriesQueries.getCategories(::mapCategory) }
     }
 
+    // الحصول على الفئات المرتبطة بمعرف المانغا
     override suspend fun getCategoriesByMangaId(mangaId: Long): List<Category> {
         return handler.awaitList {
             categoriesQueries.getCategoriesByMangaId(mangaId, ::mapCategory)
         }
     }
 
+    // الحصول على الفئات المرتبطة بمعرف المانغا كـ Flow
     override fun getCategoriesByMangaIdAsFlow(mangaId: Long): Flow<List<Category>> {
         return handler.subscribeToList {
             categoriesQueries.getCategoriesByMangaId(mangaId, ::mapCategory)
         }
     }
 
+    // إضافة فئة جديدة
     override suspend fun insert(category: Category) {
         handler.await {
             categoriesQueries.insert(
@@ -45,12 +51,14 @@ class CategoryRepositoryImpl(
         }
     }
 
+    // تحديث جزئي لفئة
     override suspend fun updatePartial(update: CategoryUpdate) {
         handler.await {
             updatePartialBlocking(update)
         }
     }
 
+    // تحديث جزئي لمجموعة من الفئات
     override suspend fun updatePartial(updates: List<CategoryUpdate>) {
         handler.await(inTransaction = true) {
             for (update in updates) {
@@ -59,6 +67,7 @@ class CategoryRepositoryImpl(
         }
     }
 
+    // تنفيذ التحديث الجزئي في المعاملة
     private fun Database.updatePartialBlocking(update: CategoryUpdate) {
         categoriesQueries.update(
             name = update.name,
@@ -68,12 +77,14 @@ class CategoryRepositoryImpl(
         )
     }
 
+    // تحديث جميع الفئات بالعلمات الجديدة
     override suspend fun updateAllFlags(flags: Long?) {
         handler.await {
             categoriesQueries.updateAllFlags(flags)
         }
     }
 
+    // حذف فئة بواسطة المعرف
     override suspend fun delete(categoryId: Long) {
         handler.await {
             categoriesQueries.delete(
@@ -82,6 +93,7 @@ class CategoryRepositoryImpl(
         }
     }
 
+    // دالة لتحويل البيانات إلى فئة
     private fun mapCategory(
         id: Long,
         name: String,
