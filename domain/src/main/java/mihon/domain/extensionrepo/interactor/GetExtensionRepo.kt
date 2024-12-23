9da097a5139ad0317c158f64e4b0d199b8390a55
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 17:44:09 +0100

    GetExtensionRepo.kt

--- domain/src/main/java/mihon/domain/extensionrepo/interactor/GetExtensionRepo.kt
+++ domain/src/main/java/mihon/domain/extensionrepo/interactor/GetExtensionRepo.kt
@@ -1,13 +1,24 @@
-package mihon.domain.extensionrepo.interactor
+package akojdad.domain.extensionrepo.interactor
 
 import kotlinx.coroutines.flow.Flow
-import mihon.domain.extensionrepo.model.ExtensionRepo
-import mihon.domain.extensionrepo.repository.ExtensionRepoRepository
+import akojdad.domain.extensionrepo.model.ExtensionRepo
+import akojdad.domain.extensionrepo.repository.ExtensionRepoRepository
 
+/**
+ * مسؤول عن استرجاع المستودعات المخزنة.
+ */
 class GetExtensionRepo(
     private val repository: ExtensionRepoRepository,
 ) {
+    /**
+     * الاشتراك في جميع مستودعات الإضافات كمصدر بيانات متدفق.
+     * @return قائمة محدثة من المستودعات.
+     */
     fun subscribeAll(): Flow<List<ExtensionRepo>> = repository.subscribeAll()
 
+    /**
+     * استرجاع جميع مستودعات الإضافات دفعة واحدة.
+     * @return قائمة بجميع المستودعات.
+     */
     suspend fun getAll(): List<ExtensionRepo> = repository.getAll()
 }
