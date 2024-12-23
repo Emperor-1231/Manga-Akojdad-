3fb9e4765fd01cbfbfad8abfa09b5dd6b014f784
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 17:30:48 +0100

    ExtensionRepoService.kt

--- domain/src/main/java/mihon/domain/extensionrepo/service/ExtensionRepoService.kt
+++ domain/src/main/java/mihon/domain/extensionrepo/service/ExtensionRepoService.kt
@@ -10,25 +10,36 @@ import mihon.domain.extensionrepo.model.ExtensionRepo
 import tachiyomi.core.common.util.lang.withIOContext
 import tachiyomi.core.common.util.system.logcat
 
+/**
+ * خدمة التعامل مع مستودعات الإضافات ضمن تطبيق Manga Akojdad.
+ * تتضمن استدعاء البيانات الخاصة بالمستودعات من الشبكة وتحويلها إلى نماذج البيانات.
+ */
 class ExtensionRepoService(
     networkHelper: NetworkHelper,
     private val json: Json,
 ) {
     val client = networkHelper.client
 
+    /**
+     * جلب تفاصيل المستودع من الرابط المحدد.
+     * @param repo رابط المستودع.
+     * @return نموذج بيانات المستودع (ExtensionRepo) إذا تم العثور عليه، أو `null` إذا حدث خطأ.
+     */
     suspend fun fetchRepoDetails(
         repo: String,
     ): ExtensionRepo? {
         return withIOContext {
             try {
                 with(json) {
+                    // تنفيذ طلب GET للحصول على بيانات المستودع من الرابط المحدد.
                     client.newCall(GET("$repo/repo.json"))
-                        .awaitSuccess()
-                        .parseAs<ExtensionRepoMetaDto>()
-                        .toExtensionRepo(baseUrl = repo)
+                        .awaitSuccess() // الانتظار حتى تنجح الاستجابة.
+                        .parseAs<ExtensionRepoMetaDto>() // تحويل البيانات المستلمة إلى نموذج DTO.
+                        .toExtensionRepo(baseUrl = repo) // تحويل الـ DTO إلى نموذج البيانات (ExtensionRepo).
                 }
             } catch (e: Exception) {
-                logcat(LogPriority.ERROR, e) { "Failed to fetch repo details" }
+                // في حال حدوث أي استثناء أو خطأ أثناء جلب البيانات، يتم تسجيل الخطأ.
+                logcat(LogPriority.ERROR, e) { "فشل في جلب تفاصيل المستودع" }
                 null
             }
         }
