158bd1e59d10d25c01f82af9fd43495528dc5d00
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-23 17:26:48 +0100

    SaveExtensionRepoException.kt

--- domain/src/main/java/mihon/domain/extensionrepo/exception/SaveExtensionRepoException.kt
+++ domain/src/main/java/mihon/domain/extensionrepo/exception/SaveExtensionRepoException.kt
@@ -3,8 +3,9 @@ package mihon.domain.extensionrepo.exception
 import java.io.IOException
 
 /**
- * Exception to abstract over SQLiteException and SQLiteConstraintException for multiplatform.
+ * استثناء يُستخدم للتعامل مع أخطاء حفظ المستودع في قاعدة البيانات.
+ * يتم تجريد الاستثناءات المتعلقة ب SQLiteException و SQLiteConstraintException لدعم الأنظمة متعددة المنصات.
  *
- * @param throwable the source throwable to include for tracing.
+ * @param throwable الاستثناء المصدر الذي يتضمنه تتبع الأخطاء.
  */
-class SaveExtensionRepoException(throwable: Throwable) : IOException("Error Saving Repository to Database", throwable)
+class SaveExtensionRepoException(throwable: Throwable) : IOException("خطأ في حفظ المستودع في قاعدة البيانات", throwable)
